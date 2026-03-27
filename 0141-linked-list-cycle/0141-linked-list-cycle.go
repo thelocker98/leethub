10723ce3f0048ed1b54ad09d4840d07f8f218d89
locker98 <coolemail45@proton.me>
2026-03-26 20:47:31 -0400

    Time: 3 ms (93.51%), Space: 6.2 MB (88.37%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	slow := head
+	fast := head
+
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+
+		if slow == fast {
+			return true
+		}
+	}
+
+	return false
+}
